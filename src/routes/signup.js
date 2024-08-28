c33b328bc8f2050a88d1b17eca339b5066341c3f
gonzjv <gonzjv@ya.ru>
2024-08-28 10:55:17 +0300

    deps: add & check body-parser

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,9 +1,19 @@
+import bodyParser from "body-parser";
 import express from "express";
+import {StatusCodes} from 'http-status-codes'
 
 const router = express.Router();
+const jsonParser = bodyParser.json();
 
-router.post("/", function (req, res) {
-    res.send("Response on /signup POST ")
+router.post("/", jsonParser, function (req, res) {
+    console.log("req.body", req.body);
+    const userExist = false;
+    
+    if (true === userExist) {
+        res.status(StatusCodes.NOT_ACCEPTABLE).send("ERROR: user already esists")
+    } else {
+        res.status(StatusCodes.OK).send("SUCCESS: user registered")
+    }
 });
 
 export default router;
