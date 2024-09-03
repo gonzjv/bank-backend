4ca0590cdcc711647feb2a2484a55eca3af2f738
gonzjv <gonzjv@ya.ru>
2024-09-03 15:53:16 +0300

    feat: mongoose to connect with mongo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import userRouter from "./routes/userRouter.js"
 import dashboardRouter from "./routes/dashboardRouter.js"
+import mongoose from "./utils/mongoose.js";
 
 const app = express();
 // use .ENV variable. dotenv package
 const port = 3000;
 
+await mongoose.connectToMongo();
+await mongoose.initDatabase();
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
 
---
+++ src/utils/mongoose.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const connectToMongo =  async () => {
+    await mongoose.connect("mongodb://localhost:27017/users");
+};
+
+
+const initDatabase =  async () => {
+    const userSchema = new mongoose.Schema({
+        email: String,
+        password: String
+    });
+    
+    const User = mongoose.model('User', userSchema);
+
+    await User.createCollection();
+    console.log("Created models:", mongoose.modelNames());
+    console.log("Colections:", await mongoose.connection.listCollections());
+};
+
+
+export default {connectToMongo, initDatabase};
